4d410e44021688409525e723275ac1d3f3cebe5f
Tyson Jones <ballisticmissle@gmail.com>
2022-04-03 13:46:09 -0600

    Start on cli

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const { readdirSync, renameSync } = require('fs');
+const { readdir, rename } = require('fs/promises');
 
-const dir = "C:/Users/Tyson/Desktop/RIPS/The Pretender/Season 01";
-const search = new RegExp("(.+?) - (S\\d{2}E\\d{2}).mkv", "g");
-const replace = "The Pretender - $2 - $1.mkv";
-
-const files = readdirSync(dir);
-
-for (const file of files) {
-    const groups = search.exec(file);
-    search.lastIndex = 0;
-    if (groups != null) {
-        const newFileName = replace.replace(/(?<!\\)(\$\d+)/g, (match) => {
-            return groups[Number(match.substring(1))]
-        });
-        renameSync(`${dir}/${file}`, `${dir}/${newFileName}`);
+module.exports = async renameFiles(dir, search, replace) {
+    const dir = "C:/Users/Tyson/Desktop/RIPS/The Pretender/Season 01";
+    const search = new RegExp("(.+?) - (S\\d{2}E\\d{2}).mkv", "g");
+    const replace = "The Pretender - $2 - $1.mkv";
+    
+    const files = readdirSync(dir);
+    
+    for (const file of files) {
+        const groups = search.exec(file);
+        search.lastIndex = 0;
+        if (groups != null) {
+            const newFileName = replace.replace(/(?<!\\)(\$\d+)/g, (match) => {
+                return groups[Number(match.substring(1))]
+            });
+            renameSync(`${dir}/${file}`, `${dir}/${newFileName}`);
+        }
     }
 }
